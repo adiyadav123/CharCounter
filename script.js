8e6ee6a9aaeb2f6ae425af252c769df65fe08f77
Aditya Yadav <ramanandyadav7650@gmail.com>
2023-09-25 21:01:26 +0530

    completed CharCounter

--- script.js
+++ script.js
@@ -8,48 +8,45 @@ var x_limit = 280;
 
 // character counter function
 const count_char = (e) => {
-    char_counter.innerText = e.target.value.length;
-}
+  char_counter.innerText = e.target.value.length;
+};
 
 // instagram-limit function
 const insta_limit_checker = (e) => {
-    insta_limit.innerText = in_limit - e.target.value.length;
-}
+  insta_limit.innerText = in_limit - e.target.value.length;
+};
 
-// twitter limit checker 
+// twitter limit checker
 const twitter_limit_checker = (e) => {
-    twitter_limit.innerText = x_limit - e.target.value.length;
-}
-// color changer 
+  twitter_limit.innerText = x_limit - e.target.value.length;
+};
+// color changer
 const color_changer = (e) => {
-    if(x_limit - e.target.value.length < 0){
-        twitter_limit.style.color = "red";
-    }else{
-        twitter_limit.style.color = "black";
-       
-    }
+  if (x_limit - e.target.value.length < 0) {
+    twitter_limit.style.color = "red";
+  } else {
+    twitter_limit.style.color = "black";
+  }
 
-    if(in_limit - e.target.value.length < 0){
-        insta_limit.style.color = "red";
-    }else{
-        insta_limit.style.color = "black";
-    }
-}
+  if (in_limit - e.target.value.length < 0) {
+    insta_limit.style.color = "red";
+  } else {
+    insta_limit.style.color = "black";
+  }
+};
 
-
-// word counter 
+// word counter
 
 const word_counter = (e) => {
-    let str = e.target.value;
-    let str_array = str.split(" ");
-    words_counter.innerText = str_array.length;
-}
+  let str = e.target.value;
+  let str_array = str.split(" ");
+  words_counter.innerText = str_array.length;
+};
 
-
-textarea.addEventListener("input", function(e){
-    count_char(e);
-    insta_limit_checker(e);
-    twitter_limit_checker(e);
-    color_changer(e);
-    word_counter(e);
-})
+textarea.addEventListener("input", function (e) {
+  count_char(e);
+  insta_limit_checker(e);
+  twitter_limit_checker(e);
+  color_changer(e);
+  word_counter(e);
+});
